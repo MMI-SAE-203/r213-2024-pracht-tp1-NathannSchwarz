6439838ce35e61302f6523bf3df06b705b6fb87e
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-04-09 14:15:03 +0200

    test const

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-//
+  const maVar = true
 </script>
 
 <template>
-  <h1 class="text-2xl">Bonjour monde !</h1>
+  <h1 v-show="maVar" class="text-2xl">Bonjour monde !</h1>
 </template>
