91d9a50b6893e59810cbef79e5834dee4a0413fe
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-04-12 10:11:59 +0200

    ajout du html dans app.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,16 +8,7 @@ import { ref } from 'vue';
 </script>
 
 <template>
-  <p>mavar : {{ maVar }}</p>
-
-  <button @click="maVar = maVar === 1 ? 0 : 1">bouton</button>
   
-  <h1 v-show="maVar === 1" class="text-2xl">maVAr vaut 1 !</h1>
-
-  <button @click="maVar = maVar === 2 ? 0 : 2">bouton 2</button>
-
-  <h1 v-show="maVar === 2" class="text-2xl">maVAr vaut 2 !</h1>
-
 
 
 
