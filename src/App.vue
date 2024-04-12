280d77b864782966f3b656acb9932931c874f805
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-04-12 10:28:49 +0200

    changement de la valeur au clic du bouton

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ const menuIsOpen = ref(true)
     </Suspense>
   </RouterView>
 
-  <button
+  <button @pointerdown="menuIsOpen = !menuIsOpen"
     aria-controls="mainNav"
     aria-expanded="true"
     class="rounded-full border-2 border-red-600 bg-red-300 px-2"
