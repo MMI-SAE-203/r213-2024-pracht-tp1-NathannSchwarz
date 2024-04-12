6ef6719b5e53bb6390a4a89d1e810f325f753640
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-04-12 11:10:56 +0200

    ajout de la boucle dans index.vue et plus dans boucle.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,6 +6,21 @@ import { ref } from 'vue';
 
   const sectionOpen = ref(0)
 
+  const sectionsData = [
+  {
+    label: 'bouton 1',
+    texte: 'Bouton uno'
+  },
+  {
+    label: 'bouton 2',
+    texte: 'Ceci est le bouton 2 !'
+  },
+  {
+    label: 'bouton 3',
+    texte: 'Ceci est le bouton 3 !'
+  }
+]
+const sectionOpen1 = ref(50)
 
 </script>
 
@@ -26,5 +41,10 @@ import { ref } from 'vue';
   </section>
 
 
+  <section v-for="({ label, texte }, key) of sectionsData" :key="key">
+        <pre class="font-mono">key : {{ key }}</pre>
+        <pre @pointerdown="sectionOpen1 = sectionOpen1 === key ? 50 : key" class="font-mono">label : {{ label }}</pre>
+        <pre  v-show="sectionOpen1 === key" class="font-mono">texte : {{ texte }}</pre>
+    </section>
 
 </template>
