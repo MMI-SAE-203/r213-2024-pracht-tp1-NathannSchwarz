57a8908618727f94102d61fad6ed042b849c8cdc
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-04-12 10:37:32 +0200

    ajout du html dans index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,6 +9,18 @@ import { ref } from 'vue';
 
 <template>
   
+  <section>
+    <button class="text-xl">bouton 1</button>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ad molestiae unde debitis aperiam quaerat consectetur, nulla, facere nisi voluptatum explicabo adipisci optio? Magnam unde inventore quas ratione dolore nostrum.</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 2</button>
+    <p>Reiciendis deserunt nobis quos, eveniet iusto inventore consequuntur eos, voluptatibus magni dicta accusamus alias optio reprehenderit ratione quo modi doloremque soluta placeat voluptate repudiandae eaque praesentium. Vel non distinctio tempore.</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 3</button>
+    <p>Obcaecati, et accusantium? Quaerat dolore fuga reprehenderit nemo inventore? Ex veritatis repellat delectus quo nostrum reprehenderit inventore quisquam architecto id non quos mollitia, minus tenetur neque! Ex quam pariatur voluptatum.</p>
+  </section>
 
 
 
